c7d3187a177b7d92cabf2998a66f48e27492c5a2
priyanka_tiwari <priyanka.tiwari@accionlabs.com>
2019-08-27 16:18:05 +0530

    first commit

---
+++ async.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/get',(req,res)=>{
+	var id = req.param('id');
+	var token = req.param('token');
+	var geo = req.param('geo');
+	res.send(id+' '+token+' '+geo )
+	console.log("request",req,"response",res)
+})
+
+app.get('/get/:version',(req,res)=>{
+	res.json({"this is req.params ":req.params.version});
+})
+
+app.param('name',(req,res,next,name)=>{
+	var modified = name + '-dude';
+	req.name = modified;
+	next();
+})
+app.get('/get/users/:name',(req,res)=>{
+	res.json({'whats up ':req.name});
+})
+
+app.post('/post_api/:id',(req,res)=>{
+	var id = req.params.id;  //if we use req.params then we should write /:id in params while post the route
+        var token = req.body.token; // used for post basically post give body of req in response.
+        var geo = req.body.geo;
+        res.json({'id':id,'token':token,'geo':geo}) // used res.json used to print json string on browser
+})
+
+app.listen(4500,()=>{
+	console.log("port running on 4500..");
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+<h3>form data</h3>
+	<label>name</label>
+	<input type="text">
+	<input type="submit" value="Save">
